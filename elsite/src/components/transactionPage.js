684365a5a770684154071f99881e17f49b40182c
angelalfau <aalfau001@citymail.cuny.edu>
2021-11-20 00:16:23 -0500

    added css to transactions

--- elsite/src/components/transactionPage.js
+++ elsite/src/components/transactionPage.js
@@ -1,15 +1,24 @@
 import React from "react";
-// import "./transactionPage.css";
+import "./transactionPage.css";
 import instance from "./axios.js";
 import { useEffect, useState } from "react";
 
 const Transaction = ({ account }) => {
     // console.log(account);
+    var formatter = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+
+        // These options are needed to round to whole numbers if that's what you want.
+        //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+        //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
+
     return (
         <tr>
             <td>{account?.name}</td>
             <td>{account?.subtype}</td>
-            <td>{account?.balances.current}</td>
+            <td>{formatter.format(account?.balances.current)}</td>
         </tr>
     );
 };
@@ -57,25 +66,26 @@ const TransactionPage = () => {
     };
 
     return (
-        <div id="background">
-            <div id="container" class="tbl-header">
+        <div id="transactionbackground">
+            <div id="transactioncontainer" class="tbl-header">
                 {loading ? (
                     <p>Currently Loading...</p>
                 ) : (
-                    <table cellPadding="0" cellSpacing="0" border="0">
-                        <thead className="tbl-header" cellPadding="0" cellSpacing="0" border="0">
+                    <table cellPadding="0" cellSpacing="0">
+                        <thead className="tbl-header" cellPadding="0" cellSpacing="0">
                             <tr>
                                 <th>Institution Name</th>
                                 <th>Account Type</th>
                                 <th>Balance</th>
                             </tr>
                         </thead>
-                        <tbody className="tbl-content" cellPadding="0" cellSpacing="0" border="0">
+                        <tbody className="tbl-content" cellPadding="0" cellSpacing="0">
                             {transactionList()}
                         </tbody>
                     </table>
                 )}
             </div>
+            <div id="endsection"></div>
         </div>
     );
 };
